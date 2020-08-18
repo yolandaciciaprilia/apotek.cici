da7bada0a7656d7edd5a89cc6b30d2eae9d41948
yolandaciciaprilia <55522847+yolandaciciaprilia@users.noreply.github.com>
2020-08-18 16:39:04 +0700

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,13 @@
+<!-- Aplikasi Persediaan Obat pada Apotek
+*******************************************************
+* Developer    : yolanda cici aprilia
+-->
+
+<?php
+session_start();
+// hapus session
+session_destroy();
+
+// alihkan ke halaman login (index.php) dan berikan alert = 2
+header('Location: index.php?alert=2');
+?>
